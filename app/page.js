9f0be97290803b7602d6157134d071c0618b7f5a
Mbring Jeremie <mbringjeremy@gmail.com>
2025-01-06 13:51:08 +0100

    add style.css

--- app/page.js
+++ app/page.js
@@ -4,15 +4,24 @@ import Header from "./components/Header";
 import TODOHero from "./components/TODOHero";
 import TODOList from "./components/TODOList";
 import Form from "./components/Form";
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const [todos, setTodos] = useState([
+    {id: self.crypto.randomUUID(), title: 'hello', is_completed: true},
+    {id: self.crypto.randomUUID(), title: 'by shoes', is_completed: true}
+  ]);
+
+  console.log(todos);
+
   return (
     <div className="wrapper">
       <Header />
-      <TODOHero todos_completed={0} total_todos={0}/>
+      <TODOHero todos_completed={0} total_todos={10}/>
       <Form />
-      <TODOList todos={[]}/>
+      <TODOList todos={todos}/>
     </div>
   );
 }
